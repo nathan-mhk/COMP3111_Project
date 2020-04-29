0687f20599053fd0e6a2182ae7ff49984650ddfa
Ethan <plpang@connect.ust.hk>
2020-04-29 17:19:38 +0800

    Add instructor list (task 1.3.3)

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -1,6 +1,7 @@
 package comp3111.coursescraper;
 
-import java.awt.event.ActionEvent;
+// import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -16,8 +17,9 @@ import javafx.scene.layout.CornerRadii;
 import javafx.geometry.Insets;
 import javafx.scene.paint.Color;
 
-import java.util.Random;
-import java.util.List;
+import java.util.*;
+import java.time.LocalTime;
+
 public class Controller {
 
     @FXML
@@ -106,6 +108,14 @@ public class Controller {
     	}
     	return true;
     }
+    
+    public void addStringFromArrayToList(List<String> list, String [] arr) {
+    	for (int j = 0; j < arr.length; j++) {
+			if (!list.contains(arr[j]) && !arr[j].contains("TBA")) {
+				list.add(arr[j]);
+			}
+		}
+    }
 
     @FXML
     void search() {
@@ -120,6 +130,9 @@ public class Controller {
     	String catalogOutput = "";
     	int courseCount = 0;
     	int sectionCount = 0;
+    	LocalTime TIME_THREE_TEN = LocalTime.of(15,10);
+    	List<String> allInstructors = new ArrayList<String>();
+    	List<String> threeTenInstructors = new ArrayList<String>();
     	
     	// loop all courses in this list
     	for (Course c : v) {
@@ -130,21 +143,48 @@ public class Controller {
     		for (int i = 0; i < c.getNumSections(); i++) {
     			Section sec = c.getSection(i);
     			newline += "\tSection: " + sec.getCode() + " (" + sec.getID() + ")\n";
+    			
     			sectionCount += 1;
+    			boolean timeflag = false;
+    			
+    			// add all new instructors to the list
+    			String [] secInstructor = sec.getInstructor().split("\n");
+    			addStringFromArrayToList(allInstructors, secInstructor);
     			
     			// loop all slots in this section
     			for (int j = 0; j < sec.getNumSlots(); ) {
         			Slot t = sec.getSlot(j);
         			j += 1;
         			newline += "\t\tSlot " + j + ": " + t + "\n";
+        			
+        			// flag true if instructors have class at three ten
+        			if(t.getDay() == 1 && t.getStart().isBefore(TIME_THREE_TEN) && t.getEnd().isAfter(TIME_THREE_TEN)) {
+        				timeflag = true;
+        			}
         		}
+    			
+    			// add instructors who have class at three ten to the list
+    			if (timeflag) {
+    				addStringFromArrayToList(threeTenInstructors, secInstructor);
+    			}
     		}
     		
     		catalogOutput += "\n" + newline;
     	}
-    	String sectionCountOutput = "Total Number of difference sections in this search: " + sectionCount + "\n";
-    	String courseCountOutput = "Total Number of Course in this search: " + courseCount + "\n";
-    	textAreaConsole.setText(courseCountOutput + sectionCountOutput + "\n" + catalogOutput);
+    	
+    	// get and sort list of instructors that are not teaching at Tu1510 
+    	allInstructors.removeAll(threeTenInstructors);
+    	allInstructors.sort(Comparator.naturalOrder());
+    	String noThreeTenInstructors = "";
+    	for (String i : allInstructors) {
+    		noThreeTenInstructors += i + "\n";
+    	}
+    	
+    	// output
+    	String sectionCountOutput = "Total Number of Different sections in this search: " + sectionCount + "\n";
+    	String courseCountOutput = "Total Number of Courses in this search: " + courseCount + "\n";
+    	String instructorListOutput = "Instructors who has teaching assignment this term but does not need to teach at Tu 3:10pm: \n" + noThreeTenInstructors + "\n";
+    	textAreaConsole.setText(courseCountOutput + sectionCountOutput + instructorListOutput + catalogOutput);
     	
     	//Add a random block on Saturday
     	AnchorPane ap = (AnchorPane)tabTimetable.getContent();
