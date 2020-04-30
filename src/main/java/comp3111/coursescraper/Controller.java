6855029bc871c1a5537dd56d7d34884967152cf8
NathanKong214 <mhkongaa@connect.ust.hk>
2020-04-30 17:31:24 +0800

    Tried to revert master

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -18,9 +18,8 @@ import javafx.scene.layout.CornerRadii;
 import javafx.geometry.Insets;
 import javafx.scene.paint.Color;
 
-import java.util.*;
-import java.time.LocalTime;
-
+import java.util.Random;
+import java.util.List;
 public class Controller {
 
     @FXML
@@ -103,13 +102,6 @@ public class Controller {
     		textAreaConsole.setText("404 page not found: invalid subject. ");
     		return false;
     	}
-    	
-    	// 404 page not found
-    	List<Course> v = scraper.scrape(textfieldURL.getText(), textfieldTerm.getText(),textfieldSubject.getText());
-    	if (v == null) {
-    		textAreaConsole.setText("404 page not found: make sure the URL, term and subject are valid. ");
-    		return false;
-    	}
     	return true;
     }
     
@@ -133,59 +125,46 @@ public class Controller {
     	List<String> allInstructors = new ArrayList<String>();
     	List<String> threeTenInstructors = new ArrayList<String>();
     	
+    	List<Course> v = scraper.scrape(textfieldURL.getText(), textfieldTerm.getText(),textfieldSubject.getText());
+    	
     	// loop all courses in this list
     	for (Course c : v) {
     		String newline = c.getTitle() + "\n";
-    		courseCount += 1;
     		
     		// loop all sections in this course
     		for (int i = 0; i < c.getNumSections(); i++) {
     			Section sec = c.getSection(i);
     			newline += "\tSection: " + sec.getCode() + " (" + sec.getID() + ")\n";
     			
-    			sectionCount += 1;
-    			boolean timeflag = false;
-    			
-    			// add all new instructors to the list
-    			String [] secInstructor = sec.getInstructor().split("\n");
-    			addStringFromArrayToList(allInstructors, secInstructor);
-    			
     			// loop all slots in this section
     			for (int j = 0; j < sec.getNumSlots(); ) {
         			Slot t = sec.getSlot(j);
         			j += 1;
         			newline += "\t\tSlot " + j + ": " + t + "\n";
-        			
-        			// flag true if instructors have class at three ten
-        			if(t.getDay() == 1 && t.getStart().isBefore(TIME_THREE_TEN) && t.getEnd().isAfter(TIME_THREE_TEN)) {
-        				timeflag = true;
-        			}
         		}
-    			
-    			// add instructors who have class at three ten to the list
-    			if (timeflag) {
-    				addStringFromArrayToList(threeTenInstructors, secInstructor);
-    			}
     		}
-    		// append the result of this course
-    		catalogOutput += "\n" + newline;
+    		
+    		textAreaConsole.setText(textAreaConsole.getText() + "\n" + newline);
     	}
     	
-    	// get and sort list of instructors that are not teaching at Tu1510 
-    	allInstructors.removeAll(threeTenInstructors);
-    	allInstructors.sort(Comparator.naturalOrder());
-    	String noThreeTenInstructors = "";
-    	for (String i : allInstructors) {
-    		noThreeTenInstructors += i + "\n";
-    	}
+    	//Add a random block on Saturday
+    	AnchorPane ap = (AnchorPane)tabTimetable.getContent();
+    	Label randomLabel = new Label("COMP1022\nL1");
+    	Random r = new Random();
+    	double start = (r.nextInt(10) + 1) * 20 + 40;
+
+    	randomLabel.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+    	randomLabel.setLayoutX(600.0);
+    	randomLabel.setLayoutY(start);
+    	randomLabel.setMinWidth(100.0);
+    	randomLabel.setMaxWidth(100.0);
+    	randomLabel.setMinHeight(60);
+    	randomLabel.setMaxHeight(60);
+    
+    	ap.getChildren().addAll(randomLabel);
     	
-    	// combine the outputs and return
-    	String sectionCountOutput = "Total Number of Different sections in this search: " + sectionCount + "\n";
-    	String courseCountOutput = "Total Number of Courses in this search: " + courseCount + "\n";
-    	String instructorListOutput = "Instructors who has teaching assignment this term but does not need to teach at Tu 3:10pm: \n" + noThreeTenInstructors + "\n";
-    	String consoleOutputResult = courseCountOutput + sectionCountOutput + instructorListOutput + catalogOutput;
     	
-    	return consoleOutputResult; 
+    	
     }
 
 	private void clearConsoleOutput() {
--- src/main/java/comp3111/coursescraper/Section.java
+++ src/main/java/comp3111/coursescraper/Section.java
@@ -47,10 +47,6 @@ public class Section {
 		sectionInstructor = instructor;
 	}
 	
-	public String getInstructor() {
-		return sectionInstructor;
-	}
-	
 	public void addSlot(Slot s) {
 		if (numSlots >= DEFAULT_MAX_SLOT)
 			return;
