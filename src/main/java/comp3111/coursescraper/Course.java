2818fc3354304332870ada0fc206cc7ce1fa624c
Ethan <plpang@connect.ust.hk>
2020-04-30 22:24:02 +0800

    Add javadoc

--- src/main/java/comp3111/coursescraper/Course.java
+++ src/main/java/comp3111/coursescraper/Course.java
@@ -1,7 +1,15 @@
 package comp3111.coursescraper;
 
+
+/**
+ * <b>A Course offered in the semester</b><br>
+ * Course stores a course, e.g.: COMP 3111 - Software Engineering. <br>
+ * A course must has a valid section. <br>
+ * Each course can technically has 1 to unlimited sections. <br>
+ * 
+ */
 public class Course {
-	private static final int DEFAULT_MAX_SLOT = 20;
+	private static final int DEFAULT_MAX_SLOT = 50;
 	
 	private String title ; 
 	private String description ;
@@ -9,29 +17,48 @@ public class Course {
 	private Section [] sections;
 	private int numSections;
 	
+	/**
+	 * This function is the constructor of Course
+	 * It initialize the array of sections and set the number 
+	 * of sections to zero 
+	 */
 	public Course() {		
 		sections = new Section[DEFAULT_MAX_SLOT];
 		for (int i = 0; i < DEFAULT_MAX_SLOT; i++) sections[i] = null;
 		numSections = 0;
 	}
 	
+	/**
+	 * This function add a section to the array of sections
+	 * @param s The section to be added
+	 */
 	public void addSection(Section s) {
 		if (numSections >= DEFAULT_MAX_SLOT)
 			return;
 		sections[numSections++] = s.clone();
 	}
 	
+	/**
+	 * This function return a specific section at a specific index
+	 * @param i The index of the section 
+	 * @return This is the section returned
+	 */
 	public Section getSection(int i) {
 		if (i >= 0 && i < numSections)
 			return sections[i];
 		return null;
 	}
 	
+	/**
+	 * This function return the last section in this course
+	 * @return The last section located in the array of sections in this course
+	 */
 	public Section getLastSection() {
 		return sections[numSections-1];
 	}
 
 	/**
+	 * This function return the course title
 	 * @return the title
 	 */
 	public String getTitle() {
@@ -39,6 +66,7 @@ public class Course {
 	}
 
 	/**
+	 * This function setup the course title
 	 * @param title the title to set
 	 */
 	public void setTitle(String title) {
@@ -46,6 +74,7 @@ public class Course {
 	}
 
 	/**
+	 * This function return the course description
 	 * @return the description
 	 */
 	public String getDescription() {
@@ -53,6 +82,7 @@ public class Course {
 	}
 
 	/**
+	 * This function setup the course description
 	 * @param description the description to set
 	 */
 	public void setDescription(String description) {
@@ -60,6 +90,7 @@ public class Course {
 	}
 
 	/**
+	 * This function return the course exclusion
 	 * @return the exclusion
 	 */
 	public String getExclusion() {
@@ -67,19 +98,26 @@ public class Course {
 	}
 
 	/**
+	 * This function setup the course exclusion
 	 * @param exclusion the exclusion to set
 	 */
 	public void setExclusion(String exclusion) {
 		this.exclusion = exclusion;
 	}
 	
+	/**
+	 * This function return the number of sections in this course
+	 * @return This is the number of sections in this course
+	 */
 	public int getNumSections() {
 		return numSections;
 	}
 	
+	/**
+	 * This function setup the number of sections in this course
+	 * @param numSections This is the number of sections to be set
+	 */
 	public void setNumSections(int numSections) {
 		this.numSections = numSections;
 	}
-	
-
 }
--- src/main/java/comp3111/coursescraper/Slot.java
+++ src/main/java/comp3111/coursescraper/Slot.java
@@ -6,6 +6,11 @@ import java.time.LocalTime;
 import java.util.Locale;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * <b>A Slot in a section</b><br>
+ * Slot stores a time slot of a section, e.g.: Tu 0900AM - 1030AM. <br>
+ * 
+ */
 public class Slot {
 	private int day;
 	private LocalTime start;
@@ -18,6 +23,10 @@ public class Slot {
 			DAYS_MAP.put(DAYS[i], i);
 	}
 
+	/**
+	 * This function duplicates a Slot object
+	 * @return This return the cloned slot
+	 */
 	@Override
 	public Slot clone() {
 		Slot s = new Slot();
@@ -27,52 +36,89 @@ public class Slot {
 		s.venue = this.venue;
 		return s;
 	}
+	
+	/**
+	 * This function return the details of a slot
+	 * @return The details in a String
+	 */
 	public String toString() {
 		return DAYS[day] + start.toString() + "-" + end.toString() + ":" + venue;
 	}
+	
+	/**
+	 * This function return the hour of the starting time
+	 * @return Starting hour
+	 */
 	public int getStartHour() {
 		return start.getHour();
 	}
+	
+	/**
+	 * This function return the minute of the starting time
+	 * @return Starting minute
+	 */
 	public int getStartMinute() {
 		return start.getMinute();
 	}
+	
+	/**
+	 * This function return the hour of the ending time
+	 * @return Ending hour
+	 */
 	public int getEndHour() {
 		return end.getHour();
 	}
+	
+	/**
+	 * This function return the minute of the ending time
+	 * @return Ending minute
+	 */
 	public int getEndMinute() {
 		return end.getMinute();
 	}
+	
 	/**
+	 * This function return the starting time
 	 * @return the start
 	 */
 	public LocalTime getStart() {
 		return start;
 	}
+	
 	/**
+	 * This funtion set the starting time
 	 * @param start the start to set
 	 */
 	public void setStart(String start) {
 		this.start = LocalTime.parse(start, DateTimeFormatter.ofPattern("hh:mma", Locale.US));
 	}
+	
 	/**
+	 * This function return the ending time
 	 * @return the end
 	 */
 	public LocalTime getEnd() {
 		return end;
 	}
+	
 	/**
+	 * This function set the ending time
 	 * @param end the end to set
 	 */
 	public void setEnd(String end) {
 		this.end = LocalTime.parse(end, DateTimeFormatter.ofPattern("hh:mma", Locale.US));
 	}
+	
 	/**
+	 * This function return the venue
 	 * @return the venue
 	 */
 	public String getVenue() {
 		return venue;
 	}
+	
 	/**
+	 * This function set the venue
 	 * @param venue the venue to set
 	 */
 	public void setVenue(String venue) {
@@ -80,12 +126,15 @@ public class Slot {
 	}
 
 	/**
+	 * This function get the day
 	 * @return the day
 	 */
 	public int getDay() {
 		return day;
 	}
+	
 	/**
+	 * This function set the day
 	 * @param day the day to set
 	 */
 	public void setDay(int day) {
