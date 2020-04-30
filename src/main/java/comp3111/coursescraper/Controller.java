60a4c15ca0ee9df161f0ae5b51e2387654f05892
NathanKong214 <mhkongaa@connect.ust.hk>
2020-04-30 17:40:32 +0800

    Revert "Merge pull request #14 from NathanKong214/ethan_dev"
    This reverts commit 985226390061c22df65047e85db31f5fa7cc2d01, reversing
    changes made to 32f3046e03e4c58420a97de2736370c0efb58c22.

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -1,7 +1,6 @@
 package comp3111.coursescraper;
 
-// import java.awt.event.ActionEvent;
-import javafx.event.ActionEvent;
+import java.awt.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -17,9 +16,8 @@ import javafx.scene.layout.CornerRadii;
 import javafx.geometry.Insets;
 import javafx.scene.paint.Color;
 
-import java.util.*;
-import java.time.LocalTime;
-
+import java.util.Random;
+import java.util.List;
 public class Controller {
 
     @FXML
@@ -99,99 +97,57 @@ public class Controller {
     		textAreaConsole.setText("404 page not found: invalid subject. ");
     		return false;
     	}
-    	
-    	// 404 page not found
-    	List<Course> v = scraper.scrape(textfieldURL.getText(), textfieldTerm.getText(),textfieldSubject.getText());
-    	if (v == null) {
-    		textAreaConsole.setText("404 page not found: make sure the URL, term and subject are valid. ");
-    		return false;
-    	}
     	return true;
     }
-    
-    public void addStringFromArrayToList(List<String> list, String [] arr) {
-    	for (int j = 0; j < arr.length; j++) {
-			if (!list.contains(arr[j]) && !arr[j].contains("TBA")) {
-				list.add(arr[j]);
-			}
-		}
-    }
-    
-    public String generateConsoleOutput(List<Course> v) {
-    	String catalogOutput = "";
-    	int courseCount = 0;
-    	int sectionCount = 0;
-    	LocalTime TIME_THREE_TEN = LocalTime.of(15,10);
-    	List<String> allInstructors = new ArrayList<String>();
-    	List<String> threeTenInstructors = new ArrayList<String>();
+
+    @FXML
+    void search() {
+    	if(!isMainURLValid()) {
+    		return;
+    	}
+    	// reset console text
+    	textAreaConsole.setText("");
+    	
+    	List<Course> v = scraper.scrape(textfieldURL.getText(), textfieldTerm.getText(),textfieldSubject.getText());
     	
     	// loop all courses in this list
     	for (Course c : v) {
     		String newline = c.getTitle() + "\n";
-    		courseCount += 1;
     		
     		// loop all sections in this course
     		for (int i = 0; i < c.getNumSections(); i++) {
     			Section sec = c.getSection(i);
     			newline += "\tSection: " + sec.getCode() + " (" + sec.getID() + ")\n";
     			
-    			sectionCount += 1;
-    			boolean timeflag = false;
-    			
-    			// add all new instructors to the list
-    			String [] secInstructor = sec.getInstructor().split("\n");
-    			addStringFromArrayToList(allInstructors, secInstructor);
-    			
     			// loop all slots in this section
     			for (int j = 0; j < sec.getNumSlots(); ) {
         			Slot t = sec.getSlot(j);
         			j += 1;
         			newline += "\t\tSlot " + j + ": " + t + "\n";
-        			
-        			// flag true if instructors have class at three ten
-        			if(t.getDay() == 1 && t.getStart().isBefore(TIME_THREE_TEN) && t.getEnd().isAfter(TIME_THREE_TEN)) {
-        				timeflag = true;
-        			}
         		}
-    			
-    			// add instructors who have class at three ten to the list
-    			if (timeflag) {
-    				addStringFromArrayToList(threeTenInstructors, secInstructor);
-    			}
     		}
-    		// append the result of this course
-    		catalogOutput += "\n" + newline;
+    		
+    		textAreaConsole.setText(textAreaConsole.getText() + "\n" + newline);
     	}
     	
-    	// get and sort list of instructors that are not teaching at Tu1510 
-    	allInstructors.removeAll(threeTenInstructors);
-    	allInstructors.sort(Comparator.naturalOrder());
-    	String noThreeTenInstructors = "";
-    	for (String i : allInstructors) {
-    		noThreeTenInstructors += i + "\n";
-    	}
-    	
-    	// combine the outputs and return
-    	String sectionCountOutput = "Total Number of Different sections in this search: " + sectionCount + "\n";
-    	String courseCountOutput = "Total Number of Courses in this search: " + courseCount + "\n";
-    	String instructorListOutput = "Instructors who has teaching assignment this term but does not need to teach at Tu 3:10pm: \n" + noThreeTenInstructors + "\n";
-    	String consoleOutputResult = courseCountOutput + sectionCountOutput + instructorListOutput + catalogOutput;
-    	
-    	return consoleOutputResult; 
-    }
+    	//Add a random block on Saturday
+    	AnchorPane ap = (AnchorPane)tabTimetable.getContent();
+    	Label randomLabel = new Label("COMP1022\nL1");
+    	Random r = new Random();
+    	double start = (r.nextInt(10) + 1) * 20 + 40;
 
-    @FXML
-    void search() {
-    	// check if the URL is valid
-    	if(!isMainURLValid()) {
-    		return;
-    	}
-    	// reset console text
-    	textAreaConsole.setText("");
-    	// scrape using scraper
-    	List<Course> v = scraper.scrape(textfieldURL.getText(), textfieldTerm.getText(),textfieldSubject.getText());
-    	// set output
-    	textAreaConsole.setText(generateConsoleOutput(v));
+    	randomLabel.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+    	randomLabel.setLayoutX(600.0);
+    	randomLabel.setLayoutY(start);
+    	randomLabel.setMinWidth(100.0);
+    	randomLabel.setMaxWidth(100.0);
+    	randomLabel.setMinHeight(60);
+    	randomLabel.setMaxHeight(60);
+    
+    	ap.getChildren().addAll(randomLabel);
+    	
+    	
+    	
     }
 
 }
