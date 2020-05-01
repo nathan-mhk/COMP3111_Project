f254d967212a2ebb9fb5e9dd27de3fc3ab01e1d1
NathanKong214 <mhkongaa@connect.ust.hk>
2020-05-02 01:06:00 +0800

    Buggy Commit: Implemented filterSlots()
    Need fixing and refactoring

--- src/main/java/comp3111/coursescraper/Filter.java
+++ src/main/java/comp3111/coursescraper/Filter.java
@@ -88,6 +88,67 @@ class Filter {
         }
     }
 
+    /**
+     * Check if the given slot matches the time filters. 
+     * Add the matching filters to machedFilters
+     * 
+     * @param slot the slot to be matched
+     * @param matchedFilters a map that contains matched filters
+     * @return true if the slot matches the time filters
+     */
+    private static boolean matchTime(Slot slot, List<String> matchedFilters) {
+        final boolean am = filters.get(AM);
+        final boolean pm = filters.get(PM);
+
+        final int middle = 12;
+        final int startTime = slot.getStartHour();
+        final int endTime = slot.getEndHour();
+
+        if ((am && pm) && ((startTime < middle) && (endTime >= middle))) {
+            /**
+             * Special case, one slot fulfilling 2 filters simultaneously
+             */
+            addMatchedFilters(matchedFilters, AM);
+            addMatchedFilters(matchedFilters, PM);
+            return true;
+        }
+        if (am && (startTime < middle)) {
+            addMatchedFilters(matchedFilters, AM);
+            return true;
+        }
+        if (pm && (startTime >= middle)) {
+            addMatchedFilters(matchedFilters, PM);
+            return true;
+        }
+        return false;        
+    }
+
+    /**
+     * Check if the given slot matches the day filters. 
+     * Add the matching filters to machedFilters
+     * 
+     * @param slot the slot to be matched
+     * @param matchedFilters a map that contains matched filters
+     * @return true if the slot matches the day filters
+     */
+    private static boolean matchDay(Slot slot, List<String> matchedFilters) {
+        final int DAYS_OFFSET_START = FILTERS_NAME.indexOf(MON);
+        final int DAYS_OFFSET_END = FILTERS_NAME.indexOf(SAT);
+
+        final int index = slot.getDay() + DAYS_OFFSET_START;
+
+        // In case the value get from slot.getDay() is out of bounds
+        if ((index >= DAYS_OFFSET_START) && (index <= DAYS_OFFSET_END)) {
+            String filterString = FILTERS_NAME.get(index);
+            boolean matched = filters.get(filterString);
+
+            if (matched) {
+                 addMatchedFilters(matchedFilters, filterString);
+                 return true;
+            }
+        }
+        return false;
+    }
 
     /**
      * Check if the labTut filter exist, whether the given section is valid
@@ -106,15 +167,110 @@ class Filter {
 
     }
 
+    /**
+     * Get a map of slot fitlers (time and day filters)
+     * @return a map of slot filters
+     */
+    private static Map<String, Boolean> getSlotFilters() {
+        Map<String, Boolean> slotFilters = new HashMap<String, Boolean>();
+
+        slotFilters.put(AM, filters.get(AM));
+        slotFilters.put(PM, filters.get(PM));
+
+        slotFilters.put(MON, filters.get(MON));
+        slotFilters.put(TUE, filters.get(TUE));
+        slotFilters.put(WED, filters.get(WED));
+        slotFilters.put(THU, filters.get(THU));
+        slotFilters.put(FRI, filters.get(FRI));
+        slotFilters.put(SAT, filters.get(SAT));
+
+        return slotFilters;
+    }
+
+    /**
+     * Check if the matched filters can fulfill the corresponding type of filters set
+     * @param matchedFilters A list of matched filters
+     * @param type The type of the list of matched filters
+     * @return true of the list of matched filters fulfills the list of applied filters of the same type
+     */
+    private static boolean filtersMatched(List<String> matchedFilters) {
+        for (Map.Entry<String, Boolean> entry : getSlotFilters().entrySet()) {
+            // Iterate until the value of current key is true, i.e. the current filter is applied
+            if (entry.getValue()) {
+                // If the current filter is inside matchedFilters, pop the one in matchedFilters
+                if (!matchedFilters.contains(entry.getKey())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Not selecting any filter == not applying those filters 
+     * "Display" sections that have ***SLOTS*** that fulfills those filters
+     * (By display it means you should modify Course, Course.sections and Course.sections.slots accordingly)
+     * Section::sectionCode (String, Lx = lecture, LAx = lab, Tx = tutorial)
+     */
+
+    /**
+     * Check if the slots in the given section matches the applied slot filters (if any). 
+     * Add the slots that fulfilled the filters to the given section
+     * @param section the section to be checked
+     * @return true if the given section have slots that fulfills the slot filters
+     * and have been successfully added to the given section. Or there are no slot filters applied
+     */
+    private static boolean filterSlots(Section section) {
+        List<Slot> slots = section.getSlots();
+
+        boolean haveTimeFilters = haveTimeFilters();
+        boolean haveDayFilters = haveDayFilters();
+
+        if (!haveTimeFilters && !haveDayFilters) {
+            // No time and day filters are applied
+            return true;
+        } else {
+            List<Slot> filteredSlots = Collections.emptyList();
+
+            // Stores slots that fulfills time/day filters
+            List<Slot> tempTimeList = Collections.emptyList();
+            List<Slot> tempDayList = Collections.emptyList();
+
             // Stores the matched filters of any slots
             List<String> matchedFilters = Collections.emptyList();
 
             for (Slot slot : slots) {
                 if (haveTimeFilters && matchTime(slot, matchedFilters)) {
+                    tempTimeList.add(slot);
+                }
+                if (haveDayFilters && matchDay(slot, matchedFilters)) {
+                   tempDayList.add(slot);
+                }
+            }
 
-            if (filtersMatched(matchedFilters, SLOT)) {
+            if (filtersMatched(matchedFilters)) {
+                if (haveTimeFilters && haveDayFilters && (tempTimeList.size() != 0) && (tempDayList.size() != 0)) {
+                    for (Slot slot : tempTimeList) {
+                        if (!filteredSlots.contains(slot)) {
+                            filteredSlots.add(slot);
+                        }
+                    }
+                    for (Slot slot : tempDayList) {
+                        if (!filteredSlots.contains(slot)) {
+                            filteredSlots.add(slot);
+                        }
+                    }
+                } else if (haveTimeFilters && !haveDayFilters && (tempTimeList.size() != 0)) {
+                    for (Slot slot : tempTimeList) {
+                        filteredSlots.add(slot);
 
                     }
+                } else if (!haveTimeFilters && haveDayFilters && (tempDayList.size() != 0)) {
+                    for (Slot slot : tempDayList) {
+                        filteredSlots.add(slot);
+                    }
+                }
+            }
 
             if (!filteredSlots.isEmpty()) {
                 section.setSlots(filteredSlots);
